c7e3ec83bcb7e3a8c77ad744b821cf2e33525623
Divéki Zsolt PhD <divekizs@ELI-ALPS.local>
2019-09-12 16:20:28 +0200

    speech topics are added

--- home_helpers.py
+++ home_helpers.py
@@ -1,4 +1,10 @@
 
+speech_topic = {
+    'light': ['switch the light on', 'turn the light off', 'make dark', 'give me light', 'lights up', 'leds down', 'switch the red led off', 'turn the diode on', 'let there be light', 'make the lights blink', 'blink the leds', 'turn the bulb up', 'switch the bulb off'],
+    'music': ['play music', 'play a song', 'play the next music', 'play the next song', 'what is the title of this song', 'who is the singer of this song', 'stop the music', 'what band is this', 'which singer is this'],
+    'weather': ['what is the weather today', 'what is the temperature outside', 'is it raining', 'is it windy outside', 'is it snowing', 'give me a weather forecast']
+}
+
 def test_any_membership(text, test):
     tmp = any(i in text for i in test)
     return tmp
@@ -30,4 +36,29 @@ class MusicPlayer:
         pass
     
     def title(self):
+        pass
+
+
+# Class for controlling the lights
+class LightControl:
+    def __init__(self, id, color):
+        self.id = id
+        self.color = color
+    
+    def switch_on(self):
+        pass
+    
+    def switch_off(self):
+        pass
+    
+    def blink(self):
+        pass
+
+
+# Class for mapping speech to action
+class SpeechMap:
+    def __init__(self, text):
+        self.text = text.lower().split()
+    
+    def find_color(self):
         pass
