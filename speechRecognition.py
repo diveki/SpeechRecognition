d1b8bb8e4bcfda10f171a7852fec06fe5ea51a72
Divéki Zsolt PhD <divekizs@ELI-ALPS.local>
2019-09-16 11:44:37 +0200

    speechRecognition file is modified to work properly

--- speechRecognition.py
+++ speechRecognition.py
@@ -13,118 +13,35 @@ import speech_recognition as sr
 from home_helpers import *
 
 
-def recognize_speech_from_mic(recognizer, microphone):
-    """Transcribe speech from recorded from `microphone`.
+music_path = './Music'
+music_player = MusicPlayer(music_path)
 
-    Returns a dictionary with three keys:
-    "success": a boolean indicating whether or not the API request was
-               successful
-    "error":   `None` if no error occured, otherwise a string containing
-               an error message if the API could not be reached or
-               speech was unrecognizable
-    "transcription": `None` if speech could not be transcribed,
-               otherwise a string containing the transcribed text
-    """
-    # check that recognizer and microphone arguments are appropriate type
-    if not isinstance(recognizer, sr.Recognizer):
-        raise TypeError("`recognizer` must be `Recognizer` instance")
+led1 = LightControl(1, 'red')
+led2 = LightControl(2, 'blue')
+led3 = LightControl(3, 'green')
+leds = [led1, led2, led3]
 
-    if not isinstance(microphone, sr.Microphone):
-        raise TypeError("`microphone` must be `Microphone` instance")
-
-    # adjust the recognizer sensitivity to ambient noise and record audio
-    # from the microphone
-    with microphone as source:
-        recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source)
-
-    # set up the response object
-    response = {
-        "success": True,
-        "error": None,
-        "transcription": None
-    }
-
-    # try recognizing the speech in the recording
-    # if a RequestError or UnknownValueError exception is caught,
-    #     update the response object accordingly
-    try:
-        response["transcription"] = recognizer.recognize_google(audio)
-    except sr.RequestError:
-        # API was unreachable or unresponsive
-        response["success"] = False
-        response["error"] = "API unavailable"
-    except sr.UnknownValueError:
-        # speech was unintelligible
-        response["error"] = "Unable to recognize speech"
-
-    return response
-
-def switchLed(*arg, **kwargs):
-    col = kwargs.get('col')
-    state = kwargs.get('state')
-    print('switch %s light %s' % (col, state))
-    
-def test_all_membership(text, test):
-    tmp = all(i in text for i in test)
-    return tmp
-
-def test_any_membership(text, test):
-    tmp = any(i in text for i in test)
-    return tmp
-
-
-def check_state(text, col):
-    tmp = set(list(col)) & set(text)
-    return list(tmp)
-
-def light_switch_on_speech(text, leds, colors = {'red', 'blue'}, *args, **kwargs):
-    text_list = text.lower().split()
-    onOff = set(['off', 'on'])
-    quitSet = set(['quit', 'exit', 'shut down', 'bye', 'good night'])
-    quit_state = test_any_membership(text_list, quitSet)
-    if quit_state:
-        print('Bye bye')
-        return True
-    else:
-        lit_value = check_state(text_list, onOff)
-        color_value = check_state(text_list, colors)
-        if all([lit_value, color_value]):
-            func = leds.get(color_value[0])
-            func(col=color_value[0], state=lit_value[0])
-            return False
-        else:
-            print('You should say `on`, `off` and the color of the light!!')
-            return False
-    
 
 rec = sr.Recognizer()
 mic = sr.Microphone()
 
-leds = {'red': switchLed,
-        'blue': switchLed
-        }
 quit_state = False
-
 quit_class = QuitClass()
 
 while True:
     input('Press enter and tell me instructions: ')
-    a = recognize_speech_from_mic(rec, mic)
-    text = a['transcription']
+    text_object = recognize_speech_from_mic(rec, mic)
+    text = text_object['transcription']
     ##################################
     # Check if the command was to quit
     quit_state = quit_class.verify(text)
-    # quit_state = light_switch_on_speech(a['transcription'], leds)
     if quit_state:
         break
     ##################################
     # Determine the context topic
-    sp = SpeechMap(text)
+    sp=SpeechMap(text, leds, music_player)
     topic = sp.find_topic()
-    if topic == 'unknown':
-        continue
-    else:
-        pass
+    sp.allocate_task(topic)
+
     
 
